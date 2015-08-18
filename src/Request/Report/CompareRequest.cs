42f2d3f44ce998f8d0f5a46aacdce9681450c320
douglasg14b <douglasg14b@gmail.com>
2015-08-18 16:52:10 -0700

    Organized Report requests into a somewhat of an inheritance hierarchy

---
+++ src/Request/Report/CompareRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpScoutNet.Request.Report
+{
+    public class CompareRequest : ReportRequest
+    {
+        /// <summary>
+        /// Previous Start Time To Compare To
+        /// </summary>
+        public DateTime? PreviousStart { get; set; }
+
+        /// <summary>
+        /// Previous End Time To Compare To
+        /// </summary>
+        public DateTime? PreviousEnd { get; set; }
+
+        public virtual NameValueCollection ToNameValueCollection()
+        {
+            if (PreviousStart.HasValue)
+                Nv.Add("start", PreviousStart.Value.ToIso8601());
+            if (PreviousEnd.HasValue)
+                Nv.Add("end", PreviousEnd.Value.ToIso8601());
+            return Nv;
+        } 
+    }
+}
--- src/Request/Report/User/UserOverallRequest.cs
+++ src/Request/Report/User/UserOverallRequest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HelpScoutNet.Request.Report.User
 {
-    public class UserOverallRequest : FieldRequest
+    public class UserOverallRequest : CompareRequest
     {
         public UserOverallRequest(int userID, DateTime? startTime, DateTime? endTime)
         {
@@ -15,36 +15,7 @@ namespace HelpScoutNet.Request.Report.User
             End = endTime;
             User = userID;
         }
-
-
-        public DateTime? Start { get; set; }
-        public DateTime? End { get; set; }
         public int User { get; set; }
-
-        /// <summary>
-        /// Previous Start Time To Compare To
-        /// </summary>
-        public DateTime? PreviousStart { get; set; }
-
-        /// <summary>
-        /// Previous End Time To Compare To
-        /// </summary>
-        public DateTime? PreviousEnd { get; set; }
-
-        /// <summary>
-        /// List of mailbox identifies to filter by mailboxes
-        /// </summary>
-        public IList<int> Mailboxes { get; set; }
-
-        /// <summary>
-        /// List of tag identifiers to filter by
-        /// </summary>
-        public IList<int> Tags { get; set; }
-
-        /// <summary>
-        /// List of conversation types (email, chat, or phone)
-        /// </summary>
-        public IList<string> Types { get; set; }
 
         /// <summary>
         /// List of folder identifiers to filter by
@@ -54,20 +25,6 @@ namespace HelpScoutNet.Request.Report.User
         public override NameValueCollection ToNameValueCollection()
         {
             base.ToNameValueCollection();
-            if (Start.HasValue)
-                Nv.Add("start", Start.Value.ToIso8601());
-            if (End.HasValue)
-                Nv.Add("end", End.Value.ToIso8601());
-            if (PreviousStart.HasValue)
-                Nv.Add("previousStart", PreviousStart.Value.ToIso8601());
-            if (PreviousEnd.HasValue)
-                Nv.Add("previousEnd", PreviousEnd.Value.ToIso8601());
-            if (Mailboxes != null && Mailboxes.Any())
-                Nv.Add("mailboxes", string.Join(",", Mailboxes));
-            if (Tags != null && Tags.Any())
-                Nv.Add("tags", string.Join(",", Tags));
-            if (Types != null && Types.Any())
-                Nv.Add("folders", string.Join(",", Types));
             if (Folders != null && Folders.Any())
                 Nv.Add("types", string.Join(",", Folders));
             if (User >= 0)
